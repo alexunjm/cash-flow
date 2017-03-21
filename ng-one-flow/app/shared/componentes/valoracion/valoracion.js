d122b6f96b9c213d835c451a812eaebcb030efac
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-21 14:32:23 +0100

    require requiere revision

--- ng-one-flow/app/shared/componentes/valoracion/valoracion.js
+++ ng-one-flow/app/shared/componentes/valoracion/valoracion.js
@@ -32,32 +32,41 @@
 			},
 		}).component('abValoracionBind', {
 			templateUrl: './app/shared/componentes/valoracion/tpl-valoracion.html',
+			require: {
+				model: 'ngModel'
+			},
 			bindings: {
-				valor: '=',
 				maximo: '@',
 				soloLectura: '@'
 			},
 			controller: function () {
 				var vm = this;
 				this.$onInit = function () {
+					this.model.$render = () => {
+						vm.value = this.model.$viewValue;
+					}
 					vm.actualizarEstrellas();
+				}
+				this.$onChange = () => {
+					this.model.$setViewValue(vm.value);
 				}
 				this.marcar = function (indice) {
 					if (vm.soloLectura && vm.soloLectura === 'true') {
 						return;
 					}
-					vm.valor.estrellas = indice + 1;
+					vm.model.estrellas = indice + 1;
 					vm.actualizarEstrellas();
 				};
 				this.actualizarEstrellas = function () {
-					if (!vm.valor) vm.valor = { estrellas: 1 };
+					if (!vm.value) vm.value = { estrellas: 1 };
 					vm.estrellas = [];
 					for (var i = 0; i < vm.maximo; i++) {
 						var estrella = {
-							marcada: (i < vm.valor.estrellas)
+							marcada: (i < vm.value.estrellas)
 						};
 						vm.estrellas.push(estrella);
 					}
+					this.model.$setViewValue(vm.value);
 				};
 			},
 		});
