e5577eee89c3b99fcb07fe84f8de050b2635c78b
elias-garcia <elias.garcia@udc.es>
2017-03-27 10:09:24 +0200

    Contacto renamed to Message in the API

--- api-flow/api/index.js
+++ api-flow/api/index.js
@@ -4,13 +4,15 @@ const usuarios = require('./usuarios.js');
 const sesiones = require('./sesiones.js');
 const maestros = require('./maestros.js');
 const movimientos = require('./movimientos.js');
-const contactos = require('./contactos.js');
+const mensajes = require('./mensajes.js');
+const items = require('./items.js');
 
 /** Función que configura las rutas de una aplicación */
 module.exports = app => {
     usuarios(app, '/api/pub/usuarios');
     sesiones(app, '/api/pub/sesiones');
     maestros(app, '/api/pub/maestros');
-    movimientos(app, '/api/priv/movimientos')
-    contactos(app, '/api/priv/contactos');
+    movimientos(app, '/api/priv/movimientos');
+    mensajes(app, '/api/priv/mensajes');
+    items(app, '/api/priv/items');
 }
